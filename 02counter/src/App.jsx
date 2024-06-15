c5e05b4f765fcd2cad337727493c7c334c7d86c7
Khushal-13 <khushaljogi7@gmail.com>
2024-06-15 18:41:39 +0530

    Second Commit

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './App.css'
+
+
+function App() {
+
+  // 1 argument : contains variable, 2 : the function responsible for updating the variable
+  let [count, setCounter] = useState(0)
+ 
+  let increase = ()=> {
+    count += 1;
+    setCounter(count)
+    console.log('Clicked', count)
+  }
+
+  let decrease = ()=> {
+    if(count > 0) {
+      setCounter(count-1)
+    }
+  }
+
+  return (
+    <>
+      <h1> Counter : {count} </h1>
+      <button onClick={increase}> Increase </button>
+      <br/><br/>
+      <button onClick={decrease}> Decrease </button>
+      
+    </>
+  )
+}
+
+export default App
