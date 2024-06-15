86e7380cd5194370d404355210f2f95fa640ff57
Khushal-13 <khushaljogi7@gmail.com>
2024-06-15 18:43:56 +0530

    Third  Commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+
+import Chai from "./Chai";
+
+function App() {
+  return (
+    // <h1> Khushal jogi learning REACT  </h1>
+    <>
+      <Chai/>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+
+function App() {
+  
+  return (
+    <> 
+      <h1> Chai aur Code || Khushal Jogi </h1>  
+      <Chai />
+    </>
+  )
+}
+
+export default App
